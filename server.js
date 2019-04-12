0039b9da06614f92aa1d3c721294d063971b6628
yesica godinez <yesica.gmata@hotmail.com>
2019-04-12 18:21:56 -0500

    added schemas

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const express = require ('express')
-const app = express()
-const port = process.env.PORT || 9000
 
-app.get ('/', (req, res) =>{
-    res.status(200).send('Hello World')
+const express = require('./node_modules/express');
+const app = express();
+const port = process.env.PORT || 9000 
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello world')
 })
 
-app.listen(port, ()=>{
-    console.log('Ready to work')
-})
+
+app.listen(port, () => {
+    console.log('Ready to work');
+});
+
+const bodyParser = require('./node_modules/body-parser');
+// initialize our express app
+
+const product = require('./routes/product.route'); // Imports routes for the products
+const mongoose = require('mongoose'); //require module moongose
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true}); //Connect to Mongo
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
